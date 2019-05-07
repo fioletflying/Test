6996fcd1113bbe9c7fbfe9d9c8e1eab301a3b190
Fei <fioletflying@hotmail.com>
2019-05-07 17:34:00 +0800

    new LeNet

---
+++ 04_CNN/LeNet5Source/Inference.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# @author   : Feifei
+# @IDE      : Pycharm
+# @file     : Inference.py
+# @time     : 2019/4/29 11:44
+
+import tensorflow as tf
+
+
+
+# 参数变量的定义
+# 输入的节点数量，等于图片的像素
+INPUT_NODE = 784
+# 输出的节点数（0-9）10个数字
+OUTPUT_NODE = 10
+
+# 配置神经网络的参数
+IMAGE_SIZE = 28
+NUM_CHANNELS = 1
+NUM_LABELS = 10
+
+#第一层卷积层的尺寸和深度
+CONV1_DEEP = 32
+CONV1_SIZE = 5
+
+#第二层卷积层的尺寸和深度
+CONV2_DEEP = 64
+CONV2_SIZE = 5
+
+#全连接层的节点个数
+FC_SIZE = 1024
+
+
+
+
+
+
---
+++ 04_CNN/LeNet5Source/test.md
@@ -0,0 +1,5 @@
+## 测试相关的数据
+
+- sfsdfs
+- 森森带伞  
+![http://plugins.jetbrains.com/]
